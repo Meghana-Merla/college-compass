eaf0a68b8995dfd849695059c3ab7197a18bccf3
Meghana-Merla <24je0654@iitism.ac.in>
2026-05-30 13:31:21 +0530

    Build College Detail Page

---
+++ src/app/college/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/prisma";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function CollegeDetailPage({
+  params,
+}: Props) {
+  const { id } = await params;
+
+  const college = await prisma.college.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!college) {
+    notFound();
+  }
+
+  return (
+    <main className="max-w-4xl mx-auto p-8">
+        <Link
+        href="/"
+        className="inline-block mb-8 text-blue-400 hover:underline"
+        >
+        ← Back to Colleges
+        </Link>
+
+      <h1 className="text-4xl font-bold mb-6">
+        {college.name}
+      </h1>
+
+
+      <div className="space-y-4">
+        <p>
+          📍 Location: {college.location}
+        </p>
+
+        <p>
+          ⭐ Rating: {college.rating}
+        </p>
+
+        <p>
+          💰 Fees: ₹{college.fees.toLocaleString()}
+        </p>
+
+        <p>
+          📈 Placements: ₹
+          {college.placements.toLocaleString()}
+        </p>
+      </div>
+    </main>
+  );
+}
--- src/components/college/CollegeCard.tsx
+++ src/components/college/CollegeCard.tsx
@@ -1,4 +1,5 @@
 import { College } from "@/types/college";
+import Link from "next/link";
 
 interface CollegeCardProps {
   college: College;
@@ -8,20 +9,22 @@ export default function CollegeCard({
   college,
 }: CollegeCardProps) {
   return (
-    <div className="border rounded-xl p-5 shadow-sm hover:shadow-md transition">
-      <h2 className="text-xl font-bold">
-        {college.name}
-      </h2>
+    <Link href={`/college/${college.id}`}>
+      <div className="border rounded-xl p-5 shadow-sm hover:shadow-md transition cursor-pointer">
+        <h2 className="text-xl font-bold">
+          {college.name}
+        </h2>
 
-      <p className="text-gray-600">
-        {college.location}
-      </p>
+        <p className="text-gray-600">
+          {college.location}
+        </p>
 
-      <div className="mt-3 space-y-1">
-        <p>⭐ Rating: {college.rating}</p>
-        <p>💰 Fees: ₹{college.fees.toLocaleString()}</p>
-        <p>📈 Placement: ₹{college.placements.toLocaleString()}</p>
+        <div className="mt-3 space-y-1">
+          <p>⭐ Rating: {college.rating}</p>
+          <p>💰 Fees: ₹{college.fees.toLocaleString()}</p>
+          <p>📈 Placement: ₹{college.placements.toLocaleString()}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
